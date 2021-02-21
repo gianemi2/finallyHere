601e1493d4c09ac56809cfaa5c84537db33bfed4
Marco Giannini <marcogiannini@Marcos-Mac-mini.local>
2021-02-21 14:01:18 +0100

    example files

---
+++ config.example.js
@@ -0,0 +1,37 @@
+//TELEGRAM DEV KEYS
+const telegramDev = {
+    NAME: 'TELEGRAM DEV',
+    TOKEN: 'TELEGRAM TOKEN',
+    CHAT_ID: 'TELEGRAM CHAT ID',
+    LEECHERS: [
+        'TELEGRAM LEECHERS CHAT ID'
+    ],
+    SPONSORS: [
+        {
+            url: 'TELEGRAM SPONSOR URL',
+            id: 'TELEGRAM SPONSOR CHAT ID',
+            products: ['PRODUCT_ID', 'PRODUCT_ID']
+        }
+    ]
+}
+
+// TELEGRAM PUBLIC KEYS
+const telegram = {
+    NAME: 'TELEGRAM PUBLIC',
+    TOKEN: 'TELEGRAM TOKEN',
+    CHAT_ID: 'TELEGRAM CHAT ID',
+    LEECHERS: [
+        'TELEGRAM LEECHERS CHAT ID'
+    ],
+    SPONSORS: [
+        {
+            url: 'TELEGRAM SPONSOR URL',
+            id: 'TELEGRAM SPONSOR CHAT ID',
+            products: ['PRODUCT_ID', 'PRODUCT_ID']
+        }
+    ]
+}
+
+const captchaAPI = '2CAPTCHA API';
+
+module.exports = { telegram, telegramDev, captchaAPI }
