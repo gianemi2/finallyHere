567bb04a960b27996ad6ed7042f73f8f8841e578
Marco Giannini <gianemi2@gmail.com>
2021-02-23 12:38:28 +0100

    SHINOBI MODE

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const fetch = require('node-fetch')
 const axios = require('axios')
 const fs = require('fs')
 const imageToBase64 = require('image-to-base64');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
 const { telegram, telegramDev, captchaAPI } = require('./config')
 const nodeArgs = process.argv.slice(2);
 
@@ -17,36 +20,40 @@ const rawStoresData = fs.readFileSync(storesJsonLocation)
 const targets = JSON.parse(rawStoresData);
 
 (async () => {
-    const options = {
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', // <- this one doesn't works in Windows
-            '--disable-gpu',
-            '--disk-cache-size=0'
-        ],
-        headless: true
-    }
-    const browser = await puppeteer.launch(options);
-    const pages = await openEveryPages(browser, targets);
-
-    console.log('Pages ready. Start to fetch.')
-    let captcha = false
-    let firstLoading = true
-
     while (true) {
-        await reloadEveryPages(pages, firstLoading)
-        firstLoading = false
+        const options = {
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // <- this one doesn't works in Windows
+                '--disable-gpu',
+                '--disk-cache-size=0',
+                '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+            ],
+            headless: true
+        }
+        console.time('loop')
+        const browser = await puppeteer.launch(options);
+        const pages = await openEveryPages(browser, targets);
+        for (const page of pages) {
+            await page.close();
+        }
+        await browser.close();
+        console.timeEnd('loop')
+        //console.log('Pages ready. Start to fetch.')
+        //let captcha = false
+        //let firstLoading = true
     }
 })();
 
+
 const openEveryPages = async (browser, targets) => {
     for (let i = 0; i < targets.length; i++) {
-        console.log(`Opening page ${i + 1} of ${targets.length}`);
+        console.log(`Opening page ${i + 1} of ${targets.length}: ${targets[i].name}`);
         try {
             if (i == 0) {
                 const pages = await browser.pages();
@@ -59,10 +66,12 @@ const openEveryPages = async (browser, targets) => {
                         interceptedRequest.continue();
                 });
 
+                console.time('open and scan')
                 await pages[0].goto(targets[0].url, { waitUntil: 'domcontentloaded', timeout: 10000 })
                 pages[0].available = targets[0].available
                 pages[0].storeIndex = 0
                 await scanPage(pages[0], targets[0])
+                console.timeEnd('open and scan');
             } else {
                 const page = await browser.newPage();
 
@@ -76,11 +85,12 @@ const openEveryPages = async (browser, targets) => {
                     else
                         interceptedRequest.continue();
                 });
-
+                console.time('open and scan')
                 await page.goto(targets[i].url, { waitUntil: 'domcontentloaded', timeout: 10000 })
                 page.available = targets[i].available
                 page.storeIndex = i
                 await scanPage(page, targets[i]);
+                console.timeEnd('open and scan')
             }
         } catch (error) {
             console.log(`Errore 1: ${error.message}`)
@@ -115,57 +125,61 @@ const reloadEveryPages = async (pages, firstLoading) => {
 }
 
 const scanPage = async (page, target) => {
-    const amazonCaptcha = await page.$('#captchacharacters');
-    if (amazonCaptcha != null) {
-        // We got a captcha
-        console.log(`${target.name} ha beccato il captcha. Provo a risolverlo...`)
-        const amazonCaptchaImageUrl = await page.$eval(('.a-row img'), node => node.src);
+    try {
+        const amazonCaptcha = await page.$('#captchacharacters');
+        if (amazonCaptcha != null) {
+            // We got a captcha
+            console.log(`${target.name} ha beccato il captcha. Provo a risolverlo...`)
+            const amazonCaptchaImageUrl = await page.$eval(('.a-row img'), node => node.src);
 
-        const captchaImageB64 = await imageToBase64(amazonCaptchaImageUrl)
-        const captchaResponse = await solveCaptcha(captchaImageB64)
+            const captchaImageB64 = await imageToBase64(amazonCaptchaImageUrl)
+            const captchaResponse = await solveCaptcha(captchaImageB64)
 
-        await page.evaluate(async ({ captchaResponse }) => {
+            await page.evaluate(async ({ captchaResponse }) => {
 
-            console.log(`CaptchaResponse is: ${captchaResponse}`);
+                console.log(`CaptchaResponse is: ${captchaResponse}`);
 
-            document.querySelector('#captchacharacters').value = captchaResponse
-            document.querySelector('form').submit()
-        }, { captchaResponse })
-    }
-
-    const isAvailable = await page.$(target.selector);
-    let price = 0
-    if (isAvailable == null) {
-        console.log(`${target.name} non disponibile`)
-        if (page.available) {
-            sendTelegramNotification(target, `${target.name} non più disponibile.`, true)
-            page.available = false;
-            updateStores(page.available, page.storeIndex)
-        }
-    } else {
-        const priceElement = await page.$('#priceblock_ourprice');
-        if (priceElement != null) {
-            price = await page.evaluate(el => el.textContent, priceElement)
-            price = parseInt(price.replace('.', ''))
-        } else {
-            price = 0
+                document.querySelector('#captchacharacters').value = captchaResponse
+                document.querySelector('form').submit()
+            }, { captchaResponse })
         }
 
-        if (price < target.price && price > target.price - 10) {
-            if (!page.available) {
-                sendTelegramNotification({ ...target, currentPrice: price })
-                console.log(`${target.name} disponibile: ${target.url}`)
-                page.available = true;
-                updateStores(page.available, page.storeIndex)
-            }
-        } else {
-            console.log(`${target.name} disponibile ma prezzo troppo ${price < target.price ? 'basso' : 'alto'}: ${price}. Il prezzo giusto sarebbe tra ${target.price - 10} e ${target.price}`)
+        const isAvailable = await page.$(target.selector);
+        let price = 0
+        if (isAvailable == null) {
+            console.log(`${target.name} non disponibile`)
             if (page.available) {
                 sendTelegramNotification(target, `${target.name} non più disponibile.`, true)
                 page.available = false;
                 updateStores(page.available, page.storeIndex)
             }
+        } else {
+            const priceElement = await page.$('#priceblock_ourprice');
+            if (priceElement != null) {
+                price = await page.evaluate(el => el.textContent, priceElement)
+                price = parseInt(price.replace('.', ''))
+            } else {
+                price = 0
+            }
+
+            if (price < target.price && price > target.price - 10) {
+                if (!page.available) {
+                    sendTelegramNotification({ ...target, currentPrice: price })
+                    console.log(`${target.name} disponibile: ${target.url}`)
+                    page.available = true;
+                    updateStores(page.available, page.storeIndex)
+                }
+            } else {
+                console.log(`${target.name} disponibile ma prezzo troppo ${price < target.price ? 'basso' : 'alto'}: ${price}. Il prezzo giusto sarebbe tra ${target.price - 10} e ${target.price}`)
+                if (page.available) {
+                    sendTelegramNotification(target, `${target.name} non più disponibile.`, true)
+                    page.available = false;
+                    updateStores(page.available, page.storeIndex)
+                }
+            }
         }
+    } catch (error) {
+        console.error(error);
     }
 }
 
